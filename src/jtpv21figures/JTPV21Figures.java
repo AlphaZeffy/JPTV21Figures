54a6ef797fd0895e8a110f1c00460b0ba1f5d537
pupil <pupil@ivkhk.ee>
2022-09-22 12:46:50 +0300

    first commit

---
+++ src/jtpv21figures/JTPV21Figures.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtpv21figures;
+
+/**
+ *
+ * @author pupil
+ */
+public class JTPV21Figures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
